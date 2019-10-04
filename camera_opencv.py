3c66d3b38e454f128318eebcd9fc813f881c4ab9
Saizo Aoyagi <uozias@gmail.com>
2019-10-04 16:51:39 +0900

    [Add] resize and quality

--- camera_opencv.py
+++ camera_opencv.py
@@ -5,7 +5,12 @@ from base_camera import BaseCamera
 
 class Camera(BaseCamera):
     video_source = 0
-
+    full_width = 0
+    full_height = 0
+    div = 0.2
+    size =  (200, 200)
+    quality = 30
+   
     def __init__(self):
         if os.environ.get('OPENCV_CAMERA_SOURCE'):
             Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
@@ -21,9 +26,19 @@ class Camera(BaseCamera):
         if not camera.isOpened():
             raise RuntimeError('Could not start camera.')
 
+        Camera.full_width = camera.get(cv2.CAP_PROP_FRAME_WIDTH) 
+        Camera.full_height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+      
+        Camera.size = (int(Camera.full_width * Camera.div), int(Camera.full_height * Camera.div))
+
         while True:
             # read current frame
             _, img = camera.read()
 
+            #Create Half Size Image
+            #new_img = cv2.resize(img, dsize=Camera.size)
+
             # encode as a jpeg image and return it
-            yield cv2.imencode('.jpg', img)[1].tobytes()
+            img = cv2.resize(img,Camera.size)
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), Camera.quality]
+            yield cv2.imencode('.jpg', img, encode_param)[1].tobytes()
